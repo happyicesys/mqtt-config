76b7ae5650ea505fbb4343e4700b8fb05d947048
happyicesys <brianlee@happyice.com.my>
2023-07-25 22:35:08 +0800

    debug api

--- app/Http/Controllers/VendMqttController.php
+++ app/Http/Controllers/VendMqttController.php
@@ -8,7 +8,7 @@ use Illuminate\Http\Request;
 
 class VendMqttController extends Controller
 {
-    public function validate(Request $request)
+    public function create(Request $request)
     {
         DataLog::create([
             'content' => $request->all()
--- routes/api.php
+++ routes/api.php
@@ -15,7 +15,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::post('/validate', [VendMqttController::class, 'validate']);
+Route::post('/validate', [VendMqttController::class, 'create']);
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();

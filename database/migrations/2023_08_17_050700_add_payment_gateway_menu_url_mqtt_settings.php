b7d18679e49d0da5c096b1142d7ad52a138df612
Brian Lee <brian@Brians-MacBook-Pro.local>
2023-08-17 13:08:01 +0800

    debug

--- database/migrations/2023_08_17_050700_add_payment_gateway_menu_url_mqtt_settings.php
+++ database/migrations/2023_08_17_050700_add_payment_gateway_menu_url_mqtt_settings.php
@@ -11,7 +11,7 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::table('mqtt_configs', function (Blueprint $table) {
+        Schema::table('mqtt_settings', function (Blueprint $table) {
             $table->string('payment_gateway_menu_url')->nullable();
         });
     }
@@ -21,7 +21,7 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::table('mqtt_configs', function (Blueprint $table) {
+        Schema::table('mqtt_settings', function (Blueprint $table) {
             $table->dropColumn('payment_gateway_menu_url');
         });
     }
